08aadda20edfd039a03107fa00392e056a031e70
luciobian <lbiancardi91@gmail.com>
2019-12-18 20:27:06 -0300

    feat: Parameters added to components

--- src/components/WeatherData.js
+++ src/components/WeatherData.js
@@ -1,8 +1,11 @@
 import React from 'react';
+import WeatherExtraInfo from './WeatherExtraInfo';
+import WeatherTemperature from './WeatherTemperature';
 
 const WeatherData = () => ( 
     <div>
-        WeatherData
+        <WeatherTemperature/>
+        <WeatherExtraInfo humidity="80" wind="10 m/s"/>
     </div>
 );
 
--- src/components/WeatherLocation.js
+++ src/components/WeatherLocation.js
@@ -4,7 +4,7 @@ import WeatherData from './WeatherData';
 
 const WeatherLocation = () => (
     <div className="App">
-        <Location></Location>
+        <Location city="Buenos Aires"></Location>
         <WeatherData></WeatherData>
     </div>
 );
