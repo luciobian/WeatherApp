9f9fb399632fbe45f690a9582bf577dd69b519af
luciobian <lbiancardi91@gmail.com>
2019-12-23 16:10:47 -0300

    style: WeatherData positioned

--- src/components/WeatherLocation/WeatherData/WeatherExtraInfo.js
+++ src/components/WeatherLocation/WeatherData/WeatherExtraInfo.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-
+import './style.css';
 const WeatherExtraInfo = ({ humidity, wind }) => (
-    <div>
-        <span>{`${humidity}% -`}</span>
-        <span>{`${wind} wind`}</span>
+    <div className="extraInfoContainer">
+        <span className="extraInfoText">{`Humedad: ${humidity}% -`}</span>
+        <span className="extraInfoText">{`Vientos: ${wind} viento`}</span>
     </div>
 );
 
