e4ba3cc0370cc03be6384386de62d0d39896ad46
luciobian <lbiancardi91@gmail.com>
2020-01-07 20:25:57 -0300

    fix: Underscore removed

--- src/components/WeatherLocation/WeatherData/WeatherExtraInfo.js
+++ src/components/WeatherLocation/WeatherData/WeatherExtraInfo.js
@@ -3,7 +3,7 @@ import PropTypes from 'prop-types';
 import './style.css';
 const WeatherExtraInfo = ({ humidity, wind }) => (
     <div className="extraInfoContainer">
-        <span className="extraInfoText">{`Humedad: ${humidity}% -`}</span>
+        <span className="extraInfoText">{`Humedad: ${humidity}% `}</span>
         <span className="extraInfoText">{`Vientos: ${wind} viento`}</span>
     </div>
 );
