1b982e954711d292c82f480fd6582dd9212f2ca3
luciobian <lbiancardi91@gmail.com>
2020-01-07 19:59:53 -0300

    style: Distribution and Title bar applied

--- src/App.js
+++ src/App.js
@@ -1,4 +1,9 @@
 import React, { Component } from 'react';
+import Paper from '@material-ui/core/Paper';
+import AppBar from '@material-ui/core/AppBar';
+import Typography from '@material-ui/core/Typography';
+import Toolbar from '@material-ui/core/Toolbar';
+import { Grid, Row, Col } from 'react-flexbox-grid';
 import LocationList from './components/WeatherLocation/LocationList';
 import './App.css';
 
@@ -14,25 +19,39 @@ const cities = [
 class App extends Component {
 
 
-  onHandleSelectedLocation = city =>{
+  onHandleSelectedLocation = city => {
     debugger;
     console.log(`onHandleSelectionLocation - ${city}`);
   }
-  render(){
-    
+  render() {
+
     return (
-        <div className="App">
-          <LocationList 
-            cities={ cities }
-            onSelectedLocation={this.onHandleSelectedLocation}></LocationList>
-        </div>
-    
-      );
-    
+      <Grid>
+        <Row>
+          <AppBar position="sticky">
+            <Toolbar >
+              <Typography 
+                color="inherit"   
+                variant="title">
+                Weather App
+              </Typography>
+            </Toolbar>
+          </AppBar>
+        </Row>
+        <Row>
+          <Col xs={12} md={6}>
+            <LocationList
+              cities={cities}
+              onSelectedLocation={this.onHandleSelectedLocation}>
+            </LocationList>
+          </Col>
+          <Col xs={12} md={6}>
+            <div className="details"></div>
+          </Col>
+        </Row>
+      </Grid>
+    );
   };
 
 }
-
-
-
 export default App;
