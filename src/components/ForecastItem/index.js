309662bc8c9a3856973d344ce320a2071fd55656
luciobian <lbiancardi91@gmail.com>
2020-01-10 19:22:18 -0300

    fix: Rename file

---
+++ src/components/ForecastItem/index.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import { PropTypes } from "prop-types";
+import  WeatherData  from './../WeatherLocation/WeatherData';
+
+
+const ForecastItem = ({ weekDay, hour, data }) => (
+    <div>
+        <div> {weekDay} Hora: {hour}hs</div>
+        <WeatherData data={data}></WeatherData>
+    </div>
+);
+
+ForecastItem.porpTypes = {
+    weekDay: PropTypes.number.isRequired,
+    hour: PropTypes.number.isRequired,
+    data: PropTypes.shape({
+        temperature: PropTypes.number.isRequired,
+        weatherState: PropTypes.string.isRequired,
+        humidity: PropTypes.number.isRequired,
+        wind: PropTypes.string.isRequired,
+    }),
+}
+
+export default ForecastItem;
+
