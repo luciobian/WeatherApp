716299b3154f2274184be7e843aaebea8732558e
luciobian <lbiancardi91@gmail.com>
2020-01-15 20:20:56 -0300

    feat(): Forecast data transformed.

--- src/services/transformForecast.js
+++ src/services/transformForecast.js
@@ -1,12 +1,27 @@
 import moment from 'moment';
+import transformWeather from './transformWeather';
+import 'moment/locale/es';
 
- 
+// moment.locale('es');
+
+const capitalize = (s) => {
+    if (typeof s !== 'string') return ''
+    return s.charAt(0).toUpperCase() + s.slice(1)
+  }
+
 const transformForecast = data => (
     data.list.filter( item =>
         moment.unix(item.dt).hour() === 6 ||
         moment.unix(item.dt).hour() === 12 ||
         moment.unix(item.dt).hour() === 18            
-    )
+    ).map(item => (
+        {
+            weekDay: capitalize(moment.unix(item.dt).format('ddd')),
+            hour: moment.unix(item.dt).hour(),
+            data: transformWeather(item),
+
+        }
+    ))
 );
 
 export default transformForecast;
